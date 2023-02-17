512f930aec1aedf233d69a54abb9d115ca26709a
Bhavsagar <bhavsagar@protonmail.com>
2023-02-17 14:42:13 +0530

    added navigation and logo container

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import { Routes, Route, Outlet } from 'react-router-dom';
-
+import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 
-const Naviagtion = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the Naviagtion bar</h1>
-      </div>
-      <Outlet/>
-    </div>
-  );
-};
+import Navigation from './routes/navigation/navigation.components';
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -20,7 +10,7 @@ const Shop = () => {
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Naviagtion />}>
+      <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
       </Route>
